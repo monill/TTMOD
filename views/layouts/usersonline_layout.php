2e88fdcb358fa72e0cc08f018a0b39068168986b
monill <juaorok@hotmail.com>
2018-01-29 15:19:26 -0200

    User Online
    user online layout

--- views/layouts/usersonline_layout.php
+++ views/layouts/usersonline_layout.php
@@ -1,6 +1,9 @@
 <?php
 $title = "Users online";
 $blockId = 'b-' . sha1($title);
+
+$db = Database::getInstance();
+$user = $db->select("SELECT `id`, `username` FROM `users` WHERE enable = 'yes' AND status = 'confirmed' AND privacy != 'strong'");
 ?>
 
 <div class="card">
@@ -9,9 +12,17 @@ $blockId = 'b-' . sha1($title);
         <a data-toggle="collapse" href="#" class="showHide" id="<?php echo $blockId; ?>" style="float: right;"></a>
     </div>
     <div class="card-body slidingDiv<?php echo $blockId; ?>">
-        <!-- content -->
-
-        <!-- end content -->
+    <!-- content -->
+        <?php if (!$user): ?>
+            <p class="text-center"> No Users Online </p>
+        <?php else: ?>
+            <?php foreach ($user as $key => $u): ?>
+                <a href="<?= url('/user/id/', $u->id); ?>">
+                    <?= $u->username . " " ?>
+                </a>
+            <?php endforeach; ?>
+        <?php endif; ?>
+    <!-- end content -->
     </div>
 </div>
 <br />
