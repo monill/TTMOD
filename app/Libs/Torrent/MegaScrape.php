893947fa3ac3e7cdb1059b81fd5a05e30c174f94
monill <juaorok@hotmail.com>
2018-02-10 14:35:48 -0200

    little increment

--- app/Libs/Torrent/MegaScrape.php
+++ app/Libs/Torrent/MegaScrape.php
@@ -8,10 +8,12 @@ use App\Libs\Helper;
 class MegaScrape
 {
     private $db;
+    private $parse;
 
     public function __construct()
     {
         $this->db = Database::getInstance();
+        $this->parse = new Parse();
     }
 
     public function __clone()
@@ -27,5 +29,34 @@ class MegaScrape
         $tor = $this->db->select("SELECT `id`, `info_hash`, `update_at` FROM `torrents` WHERE `external` = 'yes' AND `update_at` <= :olddate ORDER BY torrents.id DESC LIMIT 10", ["olddate" => $interval]);
 
         var_dump($tor);
+
+        $updir = TUPLOAD;
+        while ($tor) {
+
+            $torInfo = $this->parse->torr("$updir.$tor->id");
+
+            $announce = $torInfo[0];
+            $infohash = $torInfo[1];
+            $creationdate = $torInfo[2];
+            $internalname = $torInfo[3];
+            $torrentsize = $torInfo[4];
+            $filecount = $torInfo[5];
+            $annlist = $torInfo[6];
+            $comment = $torInfo[7];
+            $filelist = $torInfo[8];
+
+
+            $seeders = $leechers = $downloaded = null;
+
+            $annlist = array();
+
+            if ($torInfo[6]) {
+                foreach ($torInfo[6] as $ann) {
+                    $annlist[] = $ann[0];
+                }
+            } else {
+                $annlist = array($announce);
+            }
+        }
     }
 }
