0385929de3191716ca04f15062a33f0f98cea556
monill <juaorok@hotmail.com>
2018-02-18 10:43:33 -0300

    Update email.php

--- config/email.php
+++ config/email.php
@@ -2,8 +2,8 @@
 
 define('MAILER', 'smtp');
 define('SMTP_USERNAME', 'bd1693de1ed477');
-define('SMTP_PASSWORD', '3d27860db97981');
+define('SMTP_PASSWORD', '123456789');
 define('SMTP_PORT', 2525);
 define('SMTP_HOST', 'smtp.mailtrap.io');
 define('SMTP_ENCRYPTION', 'tls');
-define('FROM_MAIL', '751952e2fa-1a0ce1@inbox.mailtrap.io');
+define('FROM_MAIL', 'monill@inbox.mailtrap.io');
